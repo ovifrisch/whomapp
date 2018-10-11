6afc78d858a93abc41ce52587f1d9a620e645a80
Ovi Fritsch <oafritsch@ucdavis.edu>
2018-10-11 01:42:42 -0700

    added search location feature

---
+++ app/assets/javascripts/search_location.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function() {
+  $('#search_loc_button').click(function() {
+    querried_location = document.getElementById("search_loc_field").value
+    document.getElementById("search_loc_field").value = "";
+    address = querried_location + ", us";
+    var geocoder =  new google.maps.Geocoder();
+    geocoder.geocode( { 'address': querried_location}, function(results, status) {
+      latitude = results[0].geometry.location.lat();
+      longitude = results[0].geometry.location.lng();
+      map.setCenter({lat: latitude, lng: longitude});
+    });
+  })
+
+  $('#search_loc_field').keypress(function(e){
+    if(e.keyCode==13) {
+      e.preventDefault();
+      $('#search_loc_button').click();
+    }
+    });
+})
