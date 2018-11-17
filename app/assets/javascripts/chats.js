16e551c008c5706ded31bf1a152e39eb088a6804
Ovi Fritsch <oafritsch@ucdavis.edu>
2018-11-17 02:03:51 -0800

    map clickability around chats and some chat manipulation logic

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -14,25 +14,40 @@ function create_conversation(user_id) {
 
 //CLICK OPEN SIDEBAR
 function open_all_chats() {
-  $("#all_chat_btn").css("display", "none");
-  $("#all_chats_box").css("display", "block");
+  $("#all_chat_btn").css("display", "none")
+  $("#all_chats_box").css("display", "block")
 }
 
 //CLICK CLOSE SIDE BAR
 function toggle_all_chats() {
-  $("#all_chats_box").css("display", "none");
-  $("#all_chat_btn").css("display", "block");
+  $("#all_chats_box").css("display", "none")
+  $("#all_chat_btn").css("display", "block")
+}
+
+//CLICK TOGGLE IN CHAT WINDOW
+function toggle_chat(el) {
+  wdw = $(el).parents().eq(1)
+  wrapper = $(el).parents().eq(2)
+  btm_pnl = $(el).parents().eq(2).children().first()
+
+  wrapper.css("visibility", "hidden")
+  btm_pnl.css("visibility", "visible")
+
+
 }
 
 //CLICK BOTTOM HIDDEN CHAT
-function openChat(el) {
-  $(el).parents().eq(0).css("display", "none");
-  $(el).parents().eq(1).children().eq(1).css("display", "block");
+function open_chat(el) {
+  btm_pnl = $(el).parents().eq(0)
+  chat_wdw = $(el).parents().eq(1).children().eq(1)
+  wrapper = $(el).parents().eq(1)
+
+  wrapper.css("visibility", "visible")
 }
 
 //CLICK SIDE BAR CHAT
 function chatbox_chat_clicked(el) {
-  id = Number($(el).attr("id").substring(4,));
+  id = Number($(el).attr("id").substring(4,))
 
   $.ajax({
     url: "chatrooms/show_chat_window",
@@ -46,26 +61,26 @@ function chatbox_chat_clicked(el) {
 function close_chat(el) {
   position = Number($(el).attr("id").substr(-1))
   $(el).empty()
+  $(el).css("visibility", "hidden")
+
   for (var i = position + 1; i <= num_open_chats; i++) {
-    slide_chat(i, i - 1);
+    slide_chat(i - 1, i)
   }
   num_open_chats -= 1
 }
 
-//CLICK TOGGLE IN CHAT WINDOW
-function toggle_chat(el) {
-  $(el).parents().eq(1).css("display", "none");
-  $(el).parents().eq(2).children().first().css("display", "block");
-}
-
 // MOVE THE CONTENT OF #(chat_wrapper<idx_from>) to #(chat_wrapper<idx_to>)
-function slide_chat(idx_from, idx_to) {
-  src = "chat_wrapper" + idx_from
-  dest = "chat_wrapper" + idx_to
+function slide_chat(idx_to, idx_from) {
+  src = $("#chat_wrapper" + idx_from)
+  dest = $("#chat_wrapper" + idx_to)
 
-  $('#' + dest).empty()
-  $('#' + dest).append($('#' + src).html())
-  $('#' + src).empty()
+  dest.empty()
+  dest.append(src.html())
+
+
+  dest.css("visibility", src.css("visibility"))
+  src.empty()
+  src.css("visibility", "hidden")
 }
 
 function go_to_user(id) {
@@ -75,8 +90,8 @@ function go_to_user(id) {
     data: {id: id},
     dataType:"json",
     success: function(loc) {
-      map.setZoom(14);
-      map.panTo({lat: loc.lat, lng: loc.long});
+      map.setZoom(14)
+      map.panTo({lat: loc.lat, lng: loc.long})
     }
   });
 }
